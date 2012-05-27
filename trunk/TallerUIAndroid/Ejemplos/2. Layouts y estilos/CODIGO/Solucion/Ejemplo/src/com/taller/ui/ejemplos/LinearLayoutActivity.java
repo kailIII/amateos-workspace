3410e0b915af65f64fba7227eff77046cb079e62
rolimat@gmail.com <rolimat@gmail.com@b9caf5d6-9787-0dec-98e6-00987d35e496>
2012-05-27 20:49:26 +0000

    Ejemplos

---
+++ trunk/TallerUIAndroid/Ejemplos/2. Layouts y estilos/CODIGO/Solucion/Ejemplo/src/com/taller/ui/ejemplos/LinearLayoutActivity.java
@@ -0,0 +1,26 @@
+package com.taller.ui.ejemplos;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LinearLayoutActivity extends Activity {
+    
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear);
+        
+        // LISTENER BOTON
+        Button boton = (Button) findViewById(R.id.button1);
+        boton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "Bot—n pulsado", Toast.LENGTH_LONG).show();
+			}
+		});
+    }
+}
---
+++ trunk/TallerUIAndroid/Ejemplos/3. Patrones/EjemploMultipanel/src/com/taller/ui/ejemplos/EjemploMultipanelActivity.java
@@ -0,0 +1,21 @@
+package com.taller.ui.ejemplos;
+
+import java.io.File;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+ 
+public class EjemploMultipanelActivity extends FragmentActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+
+    }  
+    
+}
